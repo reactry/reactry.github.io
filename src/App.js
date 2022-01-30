d7dfe4dd45de61492d16aa574b702da0f2f53f45
Ankur Seth <iankurseth@gmail.com>
2022-01-30 14:57:54 +0530

    c0, tailwind and cleanup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App () {
+	return (
+		<div className="App">
+			<header className="bg-blue-500 px-8 py-12 text-center text-white font-bold">
+				<h2 className="text-4xl">Hallo Welt</h2>
+			</header>
+		</div>
+	);
+}
+
+export default App;
